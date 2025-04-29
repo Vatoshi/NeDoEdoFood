37d923ada6d5377a2e0e7744cd82c8d95f066077
Erik <erikkadyrov32@gmail.com>
2025-04-30 01:48:16 +0600

    init

---
+++ src/main/java/kg/attractor/nedoedofood/NeDoEdoFoodApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.nedoedofood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeDoEdoFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeDoEdoFoodApplication.class, args);
+    }
+
+}
